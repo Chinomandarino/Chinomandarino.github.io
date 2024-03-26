b25d697ba551f6e0ef1ed9c9557cf3d37744a1ba
Chinomandarino <omar.gonzalez22@cetis107.edu.mx>
2024-03-26 00:54:57 -0700

    mensajegenerico

--- js/orden.js
+++ js/orden.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   sortTable(0); // Llama a la función sortTable() con el índice de la columna que deseas ordenar (en este caso, 0)
 });
+
