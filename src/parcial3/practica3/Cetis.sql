704a69ecf3132fbf63f7f14dbd5c1318cb5b9642
Chinomandarino <omar.gonzalez22@cetis107.edu.mx>
2024-05-22 21:23:41 -0700

    Tabla

--- src/parcial3/practica3/Cetis.sql
+++ src/parcial3/practica3/Cetis.sql
@@ -8,13 +8,17 @@
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -27,20 +31,34 @@ SET time_zone = "+00:00";
 -- Table structure for table `alumnos`
 --
 
-CREATE TABLE `alumnos` (
-  `ID` int(11) NOT NULL,
-  `NOMBRE` varchar(100) NOT NULL,
-  `EDAD` int(11) NOT NULL,
-  `GRUPO` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `ALUMNOS` (
+  `ID` INT(11) NOT NULL,
+  `NOMBRE` VARCHAR(100) NOT NULL,
+  `EDAD` INT(11) NOT NULL,
+  `GRUPO` VARCHAR(10) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE=UTF8_BIN;
 
 --
 -- Dumping data for table `alumnos`
 --
 
-INSERT INTO `alumnos` (`ID`, `NOMBRE`, `EDAD`, `GRUPO`) VALUES
-(1, 'Miguel', 15, '1AV'),
-(2, 'jESUS', 15, '1AV');
+INSERT INTO `ALUMNOS` (
+  `ID`,
+  `NOMBRE`,
+  `EDAD`,
+  `GRUPO`
+) VALUES (
+  1,
+  'Miguel',
+  15,
+  '1AV'
+),
+(
+  2,
+  'jESUS',
+  15,
+  '1AV'
+);
 
 -- --------------------------------------------------------
 
@@ -48,19 +66,30 @@ INSERT INTO `alumnos` (`ID`, `NOMBRE`, `EDAD`, `GRUPO`) VALUES
 -- Table structure for table `empleados`
 --
 
-CREATE TABLE `empleados` (
-  `ID` int(11) NOT NULL,
-  `NOMBRE` varchar(100) NOT NULL,
-  `ESPECIALIDAD` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `EMPLEADOS` (
+  `ID` INT(11) NOT NULL,
+  `NOMBRE` VARCHAR(100) NOT NULL,
+  `ESPECIALIDAD` VARCHAR(30) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE=UTF8_BIN;
 
 --
 -- Dumping data for table `empleados`
 --
 
-INSERT INTO `empleados` (`ID`, `NOMBRE`, `ESPECIALIDAD`) VALUES
-(1, 'Luis carlos', 'Programacion'),
-(2, 'Rochin', 'Programacion');
+INSERT INTO `EMPLEADOS` (
+  `ID`,
+  `NOMBRE`,
+  `ESPECIALIDAD`
+) VALUES (
+  1,
+  'Luis carlos',
+  'Programacion'
+),
+(
+  2,
+  'Rochin',
+  'Programacion'
+);
 
 --
 -- Indexes for dumped tables
@@ -69,14 +98,12 @@ INSERT INTO `empleados` (`ID`, `NOMBRE`, `ESPECIALIDAD`) VALUES
 --
 -- Indexes for table `alumnos`
 --
-ALTER TABLE `alumnos`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `ALUMNOS` ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `empleados`
 --
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `EMPLEADOS` ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -85,16 +112,17 @@ ALTER TABLE `empleados`
 --
 -- AUTO_INCREMENT for table `alumnos`
 --
-ALTER TABLE `alumnos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `ALUMNOS` MODIFY `ID` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `empleados`
 --
-ALTER TABLE `empleados`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `EMPLEADOS` MODIFY `ID` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
